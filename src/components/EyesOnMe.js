646f741a04b72b4cfb2547308b501a22d7256dd8
Kyle Greenidge <kylegreen149@gmail.com>
2024-08-11 12:46:47 -0400

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    function onFocus() {
+        console.log('Good!')
+    }
+
+    function onBlur() {
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <button onFocus={onFocus} onBlur={onBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
